164bcaec99b3abdf003005f62b95a73453fadde7
Iordache <AIordache@romania.luxoft.com>
2020-03-31 21:57:12 +0300

    Initial code of QC with fb login

---
+++ public/js/firebase-init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA24j2A7Mqp77KXFHPW8mXR03C18N6AOcw",
+  authDomain: "quarantine-cook.firebaseapp.com",
+  databaseURL: "https://quarantine-cook.firebaseio.com",
+  projectId: "quarantine-cook",
+  storageBucket: "quarantine-cook.appspot.com",
+  messagingSenderId: "127345675736",
+  appId: "1:127345675736:web:7ed7387141e484e92cf6ad"
+};
+
+var mainApp = firebase.initializeApp(firebaseConfig);
